a4c27608d3cf8ce2e46b3a6de9d3e959a0b7476a
Ayodeji Adesegun <ayodejiadesegun20@gmail.com>
2023-01-16 23:40:08 +0100

    initial commit

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
 
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
-    const response = await fetch('https://moyini.onrender.com/', {
+    const response = await fetch('https://moyini.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
